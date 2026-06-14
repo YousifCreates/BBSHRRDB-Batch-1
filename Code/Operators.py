1476dc8d67932a23424038d8ab211f86a3717667
Muhammad Yousif <yousifcreates@gmail.com>
2026-06-14 18:27:04 +0500

    Operators slide and Code

---
+++ Code/Operators.py
@@ -0,0 +1,36 @@
+"""
+The Operators:
+
++: Add
+-: Subtract
+*: Multiply
+/: Divide
+*: Multiply
+/: Divide
+**: Square
+
+----------------------------------------------------
+
+==: Equal
+!=: NotEqual
+>: GreaterThan
+<: LessThan
+>=: GreaterThanOrEqual
+<=: LessThanOrEqual
+"""
+
+
+print(5.36 == 3.26)
+print(5.36 > 3.26)
+print(5.36 < 3.26)
+print(5.36 >= 3.26)
+print(5.36 <= 3.26)
+print(5.36 != 3.26)
+
+# Comparison Between two strings:
+print("Hello" == "Hello")
+print("Hello" == "hello")
+print("Hello" > "hello")
+print("Hello" < "hello")
+print("Hello" != "hello")
+
