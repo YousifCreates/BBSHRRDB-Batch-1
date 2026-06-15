d3c459c86f3f361f697f14b7a80b64be1ba457c8
Muhammad Yousif <yousifcreates@gmail.com>
2026-06-15 18:39:28 +0500

    Conditional Code

---
+++ Code/conditionals.py
@@ -0,0 +1,36 @@
+# age = int(input("Enter your age: "))
+#
+# if age >= 18:
+#     print("You are eligible for licence")
+#
+# else:
+#     print("You are ineligible for licence")
+
+
+
+# ____________________________________-
+marks = 82
+# if marks >=85:
+#     print("Marks >= 85")
+# elif marks < 85:
+#     print("Marks < 85")
+
+if marks >= 85:
+    print("Grade: A+")
+if marks >= 80 and marks < 85:
+    print("Grade A")
+
+
+# >= 85: A+
+# 80 - 84: A
+# 75 - 79: B+
+# 70 - 74: B
+# 65 - 69: C+
+# 60 - 64: C
+# 50 - 59: D
+# < 50: Fail
+
+
+
+
+
