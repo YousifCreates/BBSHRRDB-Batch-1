68a28185b459b44be85c4f9fdfe09f25c3d3ad5f
Muhammad Yousif <yousifcreates@gmail.com>
2026-06-16 11:31:58 +0500

    Updated Loops

---
+++ Code/loops.py
@@ -0,0 +1,10 @@
+# while True:
+#     user_input = input("Enter a number or 'exit' to stop: ': ")
+#     if user_input.lower() == "exit":
+#         break
+#     print(f"You entered: {user_input}")
+
+# ----------------------------------
+table = 15
+for i in range(1, 11):
+    print(f"{table} x {i} = {table * i}")
