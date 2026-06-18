70905371e5640634b07206d1ee0827d194640ba3
Muhammad Yousif <yousifcreates@gmail.com>
2026-06-18 20:07:48 +0500

    Update Dictionaries and Patterns

---
+++ Code/dictionary.py
@@ -0,0 +1,18 @@
+student_info = {
+    "name": "Yousif",
+    "age": 18,
+    "height": 1.5,
+    "weight": 2.5,
+}
+
+# student_info.update({"phone": "+92651189",
+# "rnando":"maioscnoiasn"})
+
+# student_info.popitem()
+
+# for keys, values in student_info.items():
+#     print(f"{keys} -> {values}")
+
+
+
+print("age" in student_info)
