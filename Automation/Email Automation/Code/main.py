fac2d5f1ea4a8f84fee60e29eae467952aa3abe5
Muhammad Yousif <yousifcreates@gmail.com>
2026-08-18 13:33:30 +0500

    Update

--- Automation/Email Automation/Code/main.py
+++ Automation/Email Automation/Code/main.py
@@ -1,21 +1,19 @@
+from dotenv import load_dotenv
 import os
 import ssl
 import smtplib
-from dotenv import load_dotenv
+
+# .\venv\Scripts\activate
 
 load_dotenv()
-
-sender_email = os.getenv("EMAIL")
+email = os.getenv("EMAIL")
 password = os.getenv("PASSWORD")
 port = os.getenv("PORT")
 smtp_server = os.getenv("SMTP_SERVER")
 
-# SSL Connection
 context = ssl.create_default_context()
 
-# Login Logic:
-
-with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-    server.login(sender_email, password)
-    print("Successfully Logged in")
-
+# Login Logic
+with smtplib.SMTP(smtp_server, port) as server:
+    server.login(email, password, context=context)
+    print("Logged in")
